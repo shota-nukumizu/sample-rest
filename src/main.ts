42503585e466720c9cd019de08b9aadb39002e5d
shota-nukumizu <author2000.1225@gmail.com>
2022-08-12 08:21:27 +0900

    set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sample REST API')
+    .setDescription('REST API Tutorial in NestJS')
+    .setVersion('0.1')
+    .build()
+  
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+  
   await app.listen(3000);
 }
 bootstrap();

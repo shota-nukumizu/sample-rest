94cbaaca38c36d8c5b8fb8fdefb1881a15c31b05
shota-nukumizu <author2000.1225@gmail.com>
2022-08-12 08:32:53 +0900

    define get method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user
   }
 
   findOne(id: number) {

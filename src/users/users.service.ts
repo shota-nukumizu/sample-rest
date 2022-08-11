4ce19da1b973253bc744fc3f388b8f54ef09ae8d
shota-nukumizu <author2000.1225@gmail.com>
2022-08-12 08:53:51 +0900

    define patchview

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,7 +20,10 @@ export class UsersService {
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    })
   }
 
   remove(id: number) {

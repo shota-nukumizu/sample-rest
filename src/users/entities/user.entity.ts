1e5edc806504ddb9cc015a70f98d706cead44ce4
shota-nukumizu <author2000.1225@gmail.com>
2022-08-12 09:02:29 +0900

    update swagger response types

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+    @ApiProperty()
+    id: number
+
+    @ApiProperty()
+    name: string
+
+    @ApiProperty({ required: false, nullable: true })
+    description: string | null
+}
